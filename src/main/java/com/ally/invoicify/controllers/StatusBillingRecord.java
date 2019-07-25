df19ed2640c40be9b4a02e4232a1109dfe180ca6
CZ48H8@ALLYIPOC.COM <CZ48H8@ALLYIPOC.COM>
2019-07-25 15:39:56 -0400

    fix updateStatus

--- src/main/java/com/ally/invoicify/controllers/StatusBillingRecord.java
+++ src/main/java/com/ally/invoicify/controllers/StatusBillingRecord.java
@@ -3,8 +3,10 @@ package com.ally.invoicify.controllers;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -30,10 +32,11 @@ public class StatusBillingRecord {
     }
 
     @PutMapping("{recordId}")
-    public BillingRecord updateStatus(@PathVariable long recordId, @RequestHeader String status ){
+    public BillingRecord updateStatus(@PathVariable long recordId, @RequestHeader String status) {
         billingRecord = recordRepository.findOne(recordId);
         billingRecord.setStatus(status);
-         return billingRecord;
+        recordRepository.save(billingRecord);
+        return billingRecord;
     }
 
 }
