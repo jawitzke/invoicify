f9b395a1ef8ad916fac62fb6acb4639a356443fc
CZ48H8@ALLYIPOC.COM <CZ48H8@ALLYIPOC.COM>
2019-07-26 13:27:23 -0400

    test alert thing

--- src/main/java/com/ally/invoicify/models/Alert.java
+++ src/main/java/com/ally/invoicify/models/Alert.java
@@ -1,11 +1,33 @@
 package com.ally.invoicify.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+@Entity
 public class Alert {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
 
     private Company client;
     private String description;
     private String message;
     private Boolean overdue;
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id=id;
+    }
 
     public Company getClient() {
         return client;
