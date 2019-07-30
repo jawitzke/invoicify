4b7111ad307a519a217236b8aea1678af29189f1
CZ48H8@ALLYIPOC.COM <CZ48H8@ALLYIPOC.COM>
2019-07-30 12:03:07 -0400

    add company record endpoint

--- src/main/java/com/ally/invoicify/controllers/BillingRecordController.java
+++ src/main/java/com/ally/invoicify/controllers/BillingRecordController.java
@@ -1,5 +1,6 @@
 package com.ally.invoicify.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,17 +9,23 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.security.core.Authentication;
+import org.springframework.beans.factory.annotation.Autowired;
 
 
 import com.ally.invoicify.models.BillingRecord;
 import com.ally.invoicify.repositories.BillingRecordRepository;
 import com.ally.invoicify.repositories.CompanyRepository;
+import com.ally.invoicify.models.Company;
+
 
 @RestController
 @RequestMapping("/api/billing-record")
 public class BillingRecordController {
-	
+
 	private BillingRecordRepository recordRepository;
+
+	@Autowired
+	private CompanyRepository companyRepository;
 
 	public BillingRecordController(BillingRecordRepository recordRepository, CompanyRepository companyRepository) {
 		this.recordRepository = recordRepository;
@@ -29,11 +36,24 @@ public class BillingRecordController {
 		return recordRepository.findAll();
 	}
 
+	@GetMapping("/company/{clientId}")
+	public List<BillingRecord> getCompanyRecords(@PathVariable long clientId) {
+		List<BillingRecord> billingRecords = recordRepository.findAll();
+		List<BillingRecord> companyRecords = new ArrayList<BillingRecord>();
+		Company company = companyRepository.findOne(clientId);
+		for (BillingRecord billingRecord : billingRecords) {
+			if (billingRecord.getClient().getId().equals(clientId)) {
+				companyRecords.add(billingRecord);
+			}
+		}
+		return companyRecords;
+	}
+
 	@DeleteMapping("{recordId}")
-	public BillingRecord voidRecord(@PathVariable long recordId, Authentication auth){
+	public BillingRecord voidRecord(@PathVariable long recordId, Authentication auth) {
 		BillingRecord billingRecord = recordRepository.findOne(recordId);
 		recordRepository.delete(billingRecord);
 		return billingRecord;
 	}
-	
+
 }
